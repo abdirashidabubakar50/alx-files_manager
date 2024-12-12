ae8d29e1b703c931fd47614cb291ae7b2f662a5d
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-12 23:57:05 +0300

    ✅ Created first API
    Created first API

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes/index.js';
+
+const app = express();
+const port = process.env.DB_PORT || 5000;
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
